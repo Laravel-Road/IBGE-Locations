824bf52c16b79373a7a07e0c9a025f45d6cb36ba
Pedro Augusto <pams.pedro@gmail.com>
2021-01-11 17:22:44 -0300

    Create install command

---
+++ src/Console/InstallCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LaravelRoad\IBGELocaltions\Console;
+
+use Illuminate\Console\Command;
+
+class InstallCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'ibge-locations:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Install the IBGE Locations resources';
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $this->callSilent('vendor:publish', ['--tag' => 'ibge-locations-config', '--force' => true]);
+        $this->call('vendor:publish', ['--tag' => 'ibge-locations--migrations', '--force' => true]);
+        $this->call('vendor:publish', ['--tag' => 'ibge-locations--seeds', '--force' => true]);
+
+        $this->call('migrate');
+        $this->call('db:seed --class LocationsTableSeeder');
+    }
+}
