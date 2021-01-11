b75d730b818cbf9b270ea3536db74d4bd6787519
Pedro Augusto <pams.pedro@gmail.com>
2021-01-11 01:47:56 -0300

    Create locations service provider

---
+++ src/Providers/LocationsServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LaravelRoad\IBGELocaltions\Providers;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+use LaravelRoad\IBGELocaltions\Services\LocationsService;
+
+class LocationsServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Relative path to the root
+     */
+    const ROOT_PATH = __DIR__ . '/../..';
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(
+            self::ROOT_PATH . '/config/ibge-locations.php', 'ibge-locations'
+        );
+
+        $this->app->bind(LocationsService::class, function () {
+            return new LocationsService();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            self::ROOT_PATH . '/config/ibge-locations.php' => config_path('ibge-locations.php'),
+        ], 'ibge-locations');
+    }
+}
